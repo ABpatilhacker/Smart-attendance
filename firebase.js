5d0c81eb5bb6422a40cca3a49ac937bae41d7c71
Anmol Bhadade <131282176+ABpatilhacker@users.noreply.github.com>
2026-01-09 00:16:19 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,13 +1,15 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyB3ytMC77uaEwdqmXgr1t-PN0z3qV_Dxi8",
-  authDomain: "smart-attendance-system-17e89.firebaseapp.com",
-  databaseURL: "https://smart-attendance-system-17e89-default-rtdb.firebaseio.com",
-  projectId: "smart-attendance-system-17e89",
-  storageBucket: "smart-attendance-system-17e89.firebasestorage.app",
-  messagingSenderId: "168700970246",
-  appId: "1:168700970246:web:392156387db81e92544a87"
-};
+<script>
+  const firebaseConfig = {
+    apiKey: "AIzaSyB3ytMC77uaEwdqmXgr1t-PN0z3qV_Dxi8",
+    authDomain: "smart-attendance-system-17e89.firebaseapp.com",
+    databaseURL: "https://smart-attendance-system-17e89-default-rtdb.firebaseio.com",
+    projectId: "smart-attendance-system-17e89",
+    storageBucket: "smart-attendance-system-17e89.appspot.com",
+    messagingSenderId: "168700970246",
+    appId: "1:168700970246:web:392156387db81e92544a87"
+  };
 
-firebase.initializeApp(firebaseConfig);
-const auth = firebase.auth();
-const database = firebase.database();
+  firebase.initializeApp(firebaseConfig);
+  const auth = firebase.auth();
+  const database = firebase.database();
+</script>
